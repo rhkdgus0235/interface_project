ab44cfdb7e541fa52a34c8c9fa36f6eae0f39cf0
kimsungkwang <skk405@naver.com>
2022-02-16 01:35:53 +0900

    quiz

---
+++ app.js
@@ -0,0 +1,126 @@
+const questionNumber = document.querySelector(".question-number");
+const questionText = document.querySelector(".question-text");
+const optionContainer = document.querySelector(".option-container");
+const answersIndicatorContainer = document.querySelector(".answers-indicator");
+const homeBox = document.querySelector(".home-box");
+const quizBox = document.querySelector(".quiz-box");
+const resultBox = document.querySelector(".result-box");
+
+let questionCounter = 0;
+let currentQuestion;
+let availableQuestions = [];
+let availableOptions = [];
+let correctAnswers = 0;
+let attemt = 0;
+
+// push the question in to availableQuestion Array
+function setAvailableQuestion() {
+  const totalQuestion = quiz.length;
+  for (let i = 0; i < totalQuestion; i++) {
+    availableQuestions.push(quiz[i]);
+  }
+}
+
+function getNewQuestion() {
+  questionNumber.innerHTML = "Question" + (questionCounter + 1) + "of" + quiz.length;
+  const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)];
+  currentQuestion = questionIndex;
+  questionText.innerHTML = currentQuestion.q;
+  const index1 = availableQuestions.indexOf(questionIndex);
+  availableQuestions.splice(index1, 1);
+  const optionlen = currentQuestion.options.length;
+  for (let i = 0; i < optionlen; i++) {
+    availableOptions.push(i);
+  }
+  optionContainer.innerHTML = "";
+  let animationDelay = 0.15;
+  //   create options in html
+  for (let i = 0; i < optionlen; i++) {
+    const optionIndex = availableOptions[Math.floor(Math.random() * availableOptions.length)];
+    const index2 = availableOptions.indexOf(optionIndex);
+    availableOptions.splice(index2, 1);
+    const option = document.createElement("div");
+    option.innerHTML = currentQuestion.options[optionIndex];
+    option.id = optionIndex;
+    option.style.animationDelay = animationDelay + "s";
+    animationDelay = animationDelay + 0.15;
+    option.className = "option";
+    optionContainer.appendChild(option);
+    option.setAttribute("onclick", "getResult(this)");
+  }
+
+  questionCounter++;
+}
+
+function getResult(element) {
+  const id = parseInt(element.id);
+  if (id === currentQuestion.answer) {
+    element.classList.add("correct");
+    updateAnswerIndicator("correct");
+    correctAnswers++;
+  } else {
+    element.classList.add("wrong");
+    updateAnswerIndicator("wrong");
+    const optionlen = optionContainer.children.length;
+    for (let i = 0; i < optionlen; i++) {
+      if (parseInt(optionContainer.children[i].id) === currentQuestion.answer) {
+        optionContainer.children[i].classList.add("correct");
+      }
+    }
+  }
+  attemt++;
+  unclickableOptions();
+}
+
+function unclickableOptions() {
+  const optionlen = optionContainer.children.length;
+  for (let i = 0; i < optionlen; i++) {
+    optionContainer.children[i].classList.add("already-answered");
+  }
+}
+function answersIndicator() {
+  answersIndicatorContainer.innerHTML = "";
+  const totalQuestion = quiz.length;
+  for (let i = 0; i < totalQuestion; i++) {
+    const indicator = document.createElement("div");
+    answersIndicatorContainer.appendChild(indicator);
+  }
+}
+
+function updateAnswerIndicator(markType) {
+  answersIndicatorContainer.children[questionCounter - 1].classList.add(markType);
+}
+
+function next() {
+  if (questionCounter === quiz.length) {
+    console.log("quiz over");
+    quizOver();
+  } else {
+    getNewQuestion();
+  }
+}
+
+function quizOver() {
+  quizBox.classList.add("hide");
+  resultBox.classList.remove("hide");
+}
+
+// function resetQuiz() {
+//   questionCounter = 0;
+//   correctAnswers = 0;
+//   attempt = 0;
+// }
+
+// ### starting point
+
+function startQuiz() {
+  homeBox.classList.add("hide");
+  quizBox.classList.remove("hide");
+  setAvailableQuestion();
+  getNewQuestion();
+  answersIndicator();
+}
+
+window.onload = function () {
+  homeBox.querySelector(".total-question").innerHTML = quiz.length;
+};
