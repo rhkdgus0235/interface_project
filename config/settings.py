e156aae70f038117d9653f82301ad266c8df33fb
soo <sujinsnla@gmail.com>
2022-02-21 15:37:02 +0900

    폴더 위치 바뀜, csv 파일과 메인 그래프 전체 수정됨, 아카이브 페이지 글 추가

--- config/settings.py
+++ config/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-STATICFILE_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
+print(STATICFILES_DIRS)
 AUTH_USER_MODEL = 'users.User'
